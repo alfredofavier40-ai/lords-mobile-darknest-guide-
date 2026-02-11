615bd00cbeefe5cf963dd51cffb7f00d6175a8c8
alfredofavier40-ai <alfredofavier40@gmail.com>
2026-02-11 01:13:27 -0500

    Create heroes.js

---
+++ data/heroes.js
@@ -0,0 +1,13 @@
+const heroes = {
+  f2p: {
+    inf: ["Rose Knight","Oath Keeper","Demon Slayer","Bombin Goblin","Child of Light"],
+    range: ["Tracker","Snow Queen","Black Crow","Bombin Goblin","Child of Light"],
+    cav: ["Rose Knight","Scarlet Bolt","Night Raven","Bombin Goblin","Child of Light"]
+  },
+
+  p2p: {
+    inf: ["Lore Weaver","Berserker","Songstress","Storm Fox","Rose Knight"],
+    range: ["Petite Devil","Mastercook","Dream Witch","Tracker","Snow Queen"],
+    cav: ["Barbarian","Storm Fox","Dream Witch","Rose Knight","Scarlet Bolt"]
+  }
+};

2da693c28af64aaf702aca19d63fd4cce874517d
alfredofavier40-ai <alfredofavier40@gmail.com>
2026-02-11 01:16:27 -0500

    Create nests.js

---
+++ data/nests.js
@@ -0,0 +1,5 @@
+const nests = {
+  inf: { counter:"range", formation:"Ranged Phalanx" },
+  range:{ counter:"cav", formation:"Cavalry Phalanx" },
+  cav:{ counter:"inf", formation:"Infantry Phalanx" }
+};
